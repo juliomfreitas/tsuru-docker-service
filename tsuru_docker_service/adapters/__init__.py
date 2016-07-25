f1cdcaa962c3dc8d8236a5a2bc54bf6c0342b8f1
Ervilis Souza <ervilisviana@gmail.com>
2016-07-25 18:28:34 -0300

    Adding get_adapter function

---
+++ tsuru_docker_service/adapters/__init__.py
@@ -0,0 +1,16 @@
+from .redis_adapter import RedisAdapter
+
+
+adapters_mapping = {'redis': RedisAdapter}
+
+
+class AdapterNotFound(BaseException):
+    pass
+
+
+def get_adapter(name):
+    adapter_class = adapters_mapping.get(name)
+    if not adapter_class:
+        raise AdapterNotFound("Adapter `{}` does not exists".format(name))
+
+    return adapter_class()
