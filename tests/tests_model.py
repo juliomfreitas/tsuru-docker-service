63850854306efbef7f60c05d7f237c07ab3cbe1d
Ervilis Souza <ervilisviana@gmail.com>
2016-07-26 13:04:59 -0300

    Adding tests of model module

---
+++ tests/tests_model.py
@@ -0,0 +1,25 @@
+import unittest
+import mongomock
+
+from tsuru_docker_service import model, adapters
+
+
+class ContainerModelTestCase(unittest.TestCase):
+
+    def test_create_from_adapter(self):
+        adapter = adapters.RedisAdapter()
+        adapter.container_id = 'dummy'
+        adapter.discover_published_port = lambda: 6379
+
+        containers = mongomock.MongoClient().db.containers
+        container = model.ContainerModel()
+        container._db = lambda: containers
+
+        self.assertEqual(containers.count(), 0)
+        container.create_from_adapter(adapter)
+        self.assertEqual(containers.count(), 1)
+
+        created = containers.find_one()
+        self.assertEqual(created['port'], 6379)
+        self.assertEqual(created['adapter'], "redis")
+        self.assertEqual(created['container_id'], "dummy")
