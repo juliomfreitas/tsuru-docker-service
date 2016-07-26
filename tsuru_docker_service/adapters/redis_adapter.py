2969202867e16d5276dad2ff41a6fe605875a01b
Ervilis Souza <ervilisviana@gmail.com>
2016-07-26 18:17:16 -0300

    Adding get_environment into RedisAdapter

--- tsuru_docker_service/adapters/redis_adapter.py
+++ tsuru_docker_service/adapters/redis_adapter.py
@@ -6,3 +6,7 @@ class RedisAdapter(BaseAdapter):
     """Adapter used to create redis container"""
 
     name = "redis"
+
+    def get_environment(self):
+        return {"REDIS_HOST": self.get_host(),
+                "REDIS_PORT": self.discover_published_port()}
