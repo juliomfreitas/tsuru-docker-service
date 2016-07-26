ea7f64847f778e1d28ebb39bbb6cf48eebd730ab
Ervilis Souza <ervilisviana@gmail.com>
2016-07-26 16:41:15 -0300

    Renaming BaseAdapter attrs

--- tsuru_docker_service/adapters/base.py
+++ tsuru_docker_service/adapters/base.py
@@ -4,7 +4,7 @@ from tsuru_docker_service import settings
 
 class BaseAdapter:
     # Image name used to find the image of the docker containers
-    image_codename = None
+    name = None
 
     # Id of container used by the Docker service
     container_id = None
@@ -16,7 +16,7 @@ class BaseAdapter:
         client = self.client()
 
         response = client.create_container(
-            image=settings.DOCKER_IMAGES[self.image_codename],
+            image=settings.DOCKER_IMAGES[self.name],
             host_config=client.create_host_config(publish_all_ports=True))
 
         self.container_id = response['Id']
@@ -36,7 +36,7 @@ class BaseAdapter:
         return result['NetworkSettings']['Ports'].values()[0][0]['HostPort']
 
     def serialize(self):
-        return {"adapter": self.image_codename,
+        return {"adapter": self.name,
                 "name": self.instance_name,
                 "container_id": self.container_id,
                 "port": self.discover_published_port()}
--- tsuru_docker_service/adapters/redis_adapter.py
+++ tsuru_docker_service/adapters/redis_adapter.py
@@ -5,4 +5,4 @@ class RedisAdapter(BaseAdapter):
 
     """Adapter used to create redis container"""
 
-    image_codename = "redis"
+    name = "redis"
